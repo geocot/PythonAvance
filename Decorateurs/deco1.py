3acb393943cc8e4ab0a16bccdd4bc7786e01cad3
Martin C <martin.couture@uqtr.ca>
2023-03-08 11:27:50 -0500

    Deco1

--- Decorateurs/deco1.py
+++ Decorateurs/deco1.py
@@ -1,6 +1,6 @@
 
 def deco(fonction):
-    def wrapper(): #La fonction doit y être sinon le décorateur démarre automatiquement.
+    def wrapper(): #La fonction doit y être sinon le décorateur démarre automatiquement avec @deco.
         print("*********")
         return fonction()
     return wrapper
