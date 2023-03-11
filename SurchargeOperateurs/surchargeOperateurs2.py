451b1e104f521de43894539d4e2797dd3ecb3ec3
Martin C <martin.couture@uqtr.ca>
2023-03-11 13:56:02 -0500

    Surcharge

---
+++ SurchargeOperateurs/surchargeOperateurs2.py
@@ -0,0 +1,35 @@
+class Point:
+    "Point"
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        self._x = value
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, value):
+        self._y = value
+
+    def __add__(self, other):
+        if type(other) == Point:
+            self._x = self._x + other.x
+            self._y = self._y + other.y
+        if type(other) == int or type(other)==float:
+            self._x = self._x + other
+            self._y = self._y + other
+
+p1 = Point(1,1)
+p2 = Point(2,2)
+p1+1 #Plante avec 1+p1 - Il faut utiliser le __radd__()
+print(p1.x,p1.y)
+
