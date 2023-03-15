7f6d281e0a85d76461e93e8cc2603141597d71b1
Martin C <martin.couture@uqtr.ca>
2023-03-15 17:39:52 -0400

    thread

---
+++ Thread/thread3.py
@@ -0,0 +1,45 @@
+<<<<<<< HEAD
+#Fonction commune
+def compteur(maximum, nom):
+    n=0
+    while n<maximum:
+        n+=1
+    print(f'Fin {nom}')
+
+################
+##Sans thread###
+################
+import time
+print('Début Sans thread')
+depart = time.time()
+compteur(100000000, 'Main')
+fin = time.time()
+print(f'Temps: {fin-depart} secs.')
+print('***********************')
+
+
+#avec thread
+import threading
+
+print('Début avec thread')
+threadA = threading.Thread(target=compteur, args=(25000000, 'A'))
+threadB = threading.Thread(target=compteur, args=(25000000, 'B'))
+threadC = threading.Thread(target=compteur, args=(25000000, 'C'))
+threadD = threading.Thread(target=compteur, args=(25000000, 'D'))
+
+depart = time.time()
+threadA.start()
+threadB.start()
+threadC.start()
+threadD.start()
+
+threadA.join()
+threadB.join()
+threadC.join()
+threadD.join()
+
+
+fin = time.time()
+print(f'Temps: {fin - depart} secs.')
+=======
+>>>>>>> parent of 594c212 (thread)
