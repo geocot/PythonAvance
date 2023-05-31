fd387a71d73d760da21938941dd171213038e298
Martin C <martin.couture@uqtr.ca>
2023-05-31 18:08:37 -0400

    Add files via upload

---
+++ Thread/module_concurrent.py
@@ -0,0 +1,26 @@
+from module_concurrent.futures import ThreadPoolExecutor
+def mul2(nombre):
+    return nombre *2
+
+liste = [x for x in range(0,10)]
+
+executor = ThreadPoolExecutor(max_workers=8)
+
+#soumission avec submit
+f = executor.submit(mul2, liste) #Affiche directement les informations
+print(f.result(timeout=5)) #[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+
+'''
+#Soumission avec map
+for result in executor.map(mul2, liste):
+   print(result)
+'''
+
+
+'''
+#Utilisation de map sans thread
+for result in map(mul2, liste):
+   print(result)
+'''
