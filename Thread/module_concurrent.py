eac653c14d886f73e0dc159d14906ed39fac4958
Martin C <martin.couture@uqtr.ca>
2023-06-15 12:43:05 -0400

    Update module_concurrent.py

--- Thread/module_concurrent.py
+++ Thread/module_concurrent.py
@@ -3,12 +3,12 @@ def mul2(nombre):
     return nombre *2
 
 liste = [x for x in range(0,10)]
-
 executor = ThreadPoolExecutor(max_workers=8)
 
 #soumission avec submit
-f = executor.submit(mul2, liste) #Affiche directement les informations
-print(f.result(timeout=5)) #[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+futures = [executor.submit(mul2, n) for n in liste] #Affiche directement les informations
+for f in futures:
+    print(f.result())# 0, 2, 4, 6, 8, 10, 12, 14, 16, 18
 
 
 
